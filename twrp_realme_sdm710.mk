510fc5da9d71757ae0dc8f54629efbf940260df4
Ij Israfil <86546896+ij-israfil@users.noreply.github.com>
2023-08-22 12:34:47 +0600

    Update twrp_realme_sdm710.mk

--- twrp_realme_sdm710.mk
+++ twrp_realme_sdm710.mk
@@ -15,7 +15,7 @@
 #
 
 # Release name
-PRODUCT_RELEASE_NAME := realme_sdm710
+PRODUCT_RELEASE_NAME := Realme_X(RMX1901)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
